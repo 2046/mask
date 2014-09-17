10fde49314c99c502e627c546c160cf52152e4e4
2046 <g@ggg.gd>
2014-09-17 18:03:26 +0800

    init

---
+++ dist/mask.js
@@ -0,0 +1,55 @@
+define(function(require, exports, module){
+    'use strict'
+    
+    var Mask, Widget, isIE6, tpl, style;
+    
+    Widget = require('widget');
+    tpl = require('./mask.tpl');
+    style = require('./mask.style');
+    isIE6 = (window.navigator.userAgent || '').toLowerCase().indexOf('msie 6') !== -1;
+    
+    Mask = Widget.extend({
+        attrs : {
+            width : null,
+            height : null,
+            styles : style,
+            opacity : null,
+            visible : false,
+            template : tpl,
+            className : 'ui-mask',
+            backgroundColor : null
+        },
+        show : function(){
+            if(!this.rendered){
+                this.render();
+                this.element.addClass(this.get('className'));
+            }
+    
+            isIE6 && this.element.css('position', 'absolute');
+            isIE6 && this.set('height', $(document).outerHeight(true));
+            this.set('visible', true);
+            return this;
+        },
+        hide : function(){
+            this.set('visible', false);
+            return this;
+        },
+        _onChangeWidth : function(val){
+            this.element.css('width', val);
+        },
+        _onChangeHeight : function(val){
+            this.element.css('height', val);
+        },
+        _onChangeOpacity : function(val){
+            this.element.css('opacity', val);
+        },
+        _onChangeBackgroundColor : function(val){
+            this.element.css('backgroundColor', val);
+        },
+        _onChangeVisible : function(val){
+            this.element[val ? 'fadeIn' : 'fadeOut']();
+        }
+    });
+    
+    module.exports = new Mask();
+});
---
+++ tests/mask-test.js
@@ -0,0 +1,58 @@
+define(function(require, exports, module){
+    'use strict'
+
+    var expect, isIE6;
+
+    expect = require('expect');
+    isIE6 = (window.navigator.userAgent || '').toLowerCase().indexOf('msie 6') !== -1;
+
+    function equals(){
+        var args = arguments;
+        expect(args[0]).to.equal(args[1]);
+    };
+
+    describe('mask', function(){
+        var mask;
+
+        beforeEach(function(){
+            mask = require('mask');
+        });
+
+        afterEach(function(){
+            mask.hide();
+        });
+
+        it('mask show', function(done){
+            mask.show();
+            expect(mask.element.is(':visible')).to.be.ok();
+            mask.hide();
+            setTimeout(function(){
+                expect(mask.element.is(':visible')).not.to.be.ok();
+                done();
+            }, 900);
+        });
+
+        it('mask default attrs', function(done){
+            mask.show();
+            var color = mask.element.css('backgroundColor');
+            equals(color === 'rgb(0, 0, 0)' || color === '#000', true);
+            setTimeout(function(){
+                equals(Number(mask.element.css('opacity')).toFixed(1), '0.2');
+                done();
+            }, 500);
+        });
+
+        it('mask set attrs', function(){
+            mask.set('backgroundColor', 'green').set('opacity', '0.3').show();
+            var color = mask.element.css('backgroundColor');
+            equals(color === 'rgb(0, 128, 0)' || color === 'green', true);
+            equals(Number(mask.element.css('opacity')).toFixed(1), '0.3');
+        });
+
+        it('mask single instance', function(){
+            var oldMask = mask;
+            var newMask = require('mask');
+            expect(oldMask).to.be(newMask);
+        });
+    });
+});
