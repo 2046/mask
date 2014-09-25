fbdf6975ab874ab08930aae7b9f8c8f847dff897
2046 <g@ggg.gd>
2014-09-25 16:40:12 +0800

    Optimization

--- dist/mask.js
+++ dist/mask.js
@@ -14,27 +14,8 @@ define(function(require, exports, module){
             className : null
         },
         init : function(){
-            this.element.addClass(this.get('className'));
             isIE6 && this.element.css('position', 'absolute');
             isIE6 && this.set('height', $(document).outerHeight(true));
-        },
-        show : function(){
-            if(!this.rendered){
-                this.render();
-            }
-    
-            this.set('visible', true);
-            return this;
-        },
-        hide : function(){
-            this.set('visible', false);
-            return this;
-        },
-        _onChangeClassName : function(val){
-            this.element.addClass(val);
-        },
-        _onChangeVisible : function(val){
-            this.element[val ? 'fadeIn' : 'fadeOut']();
         }
     });
     
